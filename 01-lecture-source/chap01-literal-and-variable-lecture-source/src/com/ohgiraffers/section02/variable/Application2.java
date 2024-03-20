0b97e22a175e403b25b0b41ce6a0cd9ae655f07b
DESKTOP-F4BC13L\hi <xodud6809@naver.com>
2024-03-20 15:56:11 +0900

    comit

--- 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section02/variable/Application2.java
+++ 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section02/variable/Application2.java
@@ -1,4 +1,21 @@
 package com.ohgiraffers.section02.variable;
 
 public class Application2 {
+    public static void main(String[] args) {
+        /*  수업목표/ 변수를 선언하고 값을 할당하여 사용할 수 있다. */
+        /*  변수를 사용하는 방법
+        *   1. 변수를 준비한다(선언)
+        *   2. 변수의 값을 대입한다(값 대입 및 초기화)
+        *   3. 변수를 사용한다.*/
+
+        /*  자료형이란?
+        *   다양한 값을 형태별로 어느 정도의 크기를 하나의 값으로 취급할 것인지 미리 Compiler와 약속한 키워드이다.
+        *   예) 앞에서 사용한 int 자료형은 정수를 4byte만큼 읽어서 하나의 값으로 취급하겠다는 약속이다. */
+
+
+
+    }
+
+
+
 }
